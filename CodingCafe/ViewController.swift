c4dfbde86f93f11e483fd915dbfad00107913c30
余佳恆 <nurockplayer@gmail.com>
2016-12-20 09:30:32 +0800

    init Mapkit & Alamofire

---
+++ CodingCafe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CodingCafe
+//
+//  Created by 余佳恆 on 2016/12/20.
+//  Copyright © 2016年 icdt. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+project 'CodingCafe.xcodeproj'
+
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'CodingCafe' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CodingCafe
+  pod 'Alamofire'
+
+end
